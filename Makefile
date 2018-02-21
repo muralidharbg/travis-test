7d0c7b1ba986b5528ab56d976dbeeda4f865b3a1
Muralidhar <Muralidhar Gopinath>
2018-02-21 00:43:07 -0500

    added input, output and makefile

---
+++ Makefile
@@ -0,0 +1,13 @@
+all:
+	g++ hello_world.c -o hello_world
+
+test1: 
+	./hello_world < input1.txt > output1.txt
+	diff test1.expected output1.txt > /dev/null
+
+test2: 
+	./hello_world < input2.txt > output2.txt
+	diff test2.expected output2.txt
+
+clean:
+	rm hello_world output*.txt
--- hello_world.c
+++ hello_world.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <string.h>
+#define BUFFERSIZE 100
 int main()
 {
    // printf() displays the string inside quotation
-   printf("Hello, World!\n");
+   char name[BUFFERSIZE];
+//   fgets(name,BUFFERSIZE,stdin);
+//   char name;
+   scanf("%[^\n]s",name);
+   if(name[0] == '\0'){
+      printf("Hello, World!\n");
+   } else{
+      printf("Hello, %s!\n",name);
+   }
    return 0;
 }
