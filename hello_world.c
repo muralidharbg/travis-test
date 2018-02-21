a74f5fe7fe072bec9a4e2511b1f0128564e4b495
Muralidhar <Muralidhar Gopinath>
2018-02-21 01:17:40 -0500

    name initialized

--- hello_world.c
+++ hello_world.c
@@ -4,14 +4,13 @@
 int main()
 {
    // printf() displays the string inside quotation
-   char name[BUFFERSIZE];
+   char name[BUFFERSIZE] = {'\0'};
 //   fgets(name,BUFFERSIZE,stdin);
 //   char name;
    scanf("%[^\n]s",name);
    if(name[0] == '\0'){
       printf("Hello, World!\n");
    } else{
-   		printf("asdasd\n");
       printf("Hello, %s!\n",name);
    }
    return 0;
